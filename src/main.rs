39c5f6ed88045f6dc30491f55ba63c0e9c7bc227
gillor <lorenz.gillner@hs-wismar.de>
2025-02-13 18:20:44 +0100

    Add more PMD functions

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,10 @@
 mod pmd;
 
-use std::{env, io};
-use std::thread;
 use std::io::{Read, Write};
-use std::mem::transmute;
-use chrono::Local;
 use std::time::Duration;
 use clap::{Arg, Command};
-use env_logger;
+// use env_logger;
 use serialport;
-use serialport::SerialPort;
-use crate::pmd::{SensorStruct, DeviceIdStruct, ContTxStruct, ConfigStruct, ConfigStructV5};
 
 fn main() {
     /* Set up command line options */
@@ -27,6 +21,11 @@ fn main() {
             .value_name("BAUDRATE")
             .help("Baud rate to use, e.g. 115200")
             .default_value("115200"))
+        .arg(Arg::new("prime")
+            .short('P')
+            .long("prime")
+            .help("Terminate potentially running continuous TX")
+            .action(clap::ArgAction::SetTrue))
         .arg(Arg::new("verbose")
             .short('v')
             .long("verbose")
@@ -37,22 +36,32 @@ fn main() {
     /* Dispatch command line options */
     let port_name = args.get_one::<String>("port").unwrap();
     let baudrate: u32 = args.get_one::<String>("baudrate").unwrap().parse().expect("Invalid baud rate");
-    let log_level = match args.get_count("verbose") {
-        0 => "warn",
-        1 => "info",
-        _ => "debug",
-    };
+    let prime: bool = args.get_flag("prime");
+    // let log_level = match args.get_count("verbose") {
+    //     0 => "warn",
+    //     1 => "info",
+    //     _ => "debug",
+    // };
 
     /* Configure verbosity level */
-    env::set_var("RUST_LOG", log_level);
-    env_logger::Builder::new()
-        .format(|buf, record| {
-            writeln!(buf, "{} [{}]: {}", Local::now().to_string(), record.level(), record.args())
-        })
-        .init();
+    // env::set_var("RUST_LOG", log_level);
+    // env_logger::Builder::new()
+    //     .format(|buf, record| {
+    //         writeln!(buf, "{} [{}]: {}", Local::now().to_string(), record.level(), record.args())
+    //     })
+    //     .init();
+
+    /* Check serial port validity */
+    let available_ports = serialport::available_ports().unwrap();
+    let is_valid_port = available_ports.iter().any(|port_info| &port_info.port_name == port_name);
+
+    if !is_valid_port {
+        println!("Invalid port name: {}", port_name);
+        std::process::exit(1);
+    }
 
     /* Give the user some feedback */
-    println!("Selected device {} at baud rate {}", port_name, baudrate);
+    println!("Selecting device {} at baud rate {}", port_name, baudrate); // TODO debug!
 
     /* Initialize serial connection */
     let mut port = match serialport::new(port_name, baudrate)
@@ -64,141 +73,25 @@ fn main() {
         Err(e) => panic!("Unable to open serial port: {}", e),
     };
 
-    let mut calibration: [i8; pmd::PMD_ADC_CH_NUM] = [0; pmd::PMD_ADC_CH_NUM];
+    /* "Global" calibration storage */
+    let mut calibration: [[i8; 2]; pmd::PMD_ADC_CH_NUM] = [[0, 0]; pmd::PMD_ADC_CH_NUM];
 
-    test_connection(&mut *port);
-    prime_connection(&mut *port);
-    read_calibration(&mut *port, &mut calibration);
-}
+    /* Send welcome message */
+    pmd::welcome(&mut *port);
 
-fn test_connection(port: &mut dyn SerialPort) {
-    /* Sanity check */
-    match port.write(&[pmd::CMD_WELCOME_MESSAGE]) {
-        Ok(_) => println!("Successfully wrote to device"),
-        Err(ref e) if e.kind() == io::ErrorKind::TimedOut => println!("Timed out while writing to device"),
-        Err(e) => panic!("Error while writing to device: {}", e),
+    /* Check sensors */
+    pmd::read_sensors(&mut *port);
+
+    /* Stop previously started continuous TX */
+    if prime {
+        pmd::prime_connection(&mut *port);
     }
 
-    port.flush().unwrap();
+    /* Read and store device calibration parameters */
+    pmd::read_calibration(&mut *port, &mut calibration);
 
-    let mut welcome_buffer = [0u8; 17];
+    let sensor_data = pmd::read_sensor_values(&mut *port);
+    println!("{:?}", sensor_data);
 
-    match port.read_exact(&mut welcome_buffer) {
-        Ok(_) => {
-            assert_eq!(&welcome_buffer, pmd::PMD_RESPONSE);
-            println!("Received response from device: {}", std::str::from_utf8(&welcome_buffer).unwrap());
-        },
-        Err(e) => panic!("Error while reading from device: {}", e),
-    }
-
-    /* Sensor check */
-    match port.write(&[pmd::CMD_READ_SENSORS]) {
-        Ok(_) => println!("Successfully wrote to device"),
-        Err(e) => panic!("Error while writing to device: {}", e),
-    }
-
-    port.flush().unwrap();
-
-    let mut sensor_buffer = [0u8; size_of::<SensorStruct>()];
-
-    println!("Expecting {} bytes of data", size_of::<SensorStruct>());
-
-    match port.read_exact(&mut sensor_buffer) {
-        Ok(_) => println!("Received {} bytes", size_of::<SensorStruct>()),
-        Err(e) => panic!("Error while reading from device: {}", e),
-    }
-
-    let sensors: SensorStruct = unsafe {
-        transmute(sensor_buffer)
-    };
-
-    for sen in sensors.sensor {
-        println!("> Hello from sensor {}: {} V, {} A, {} W", std::str::from_utf8(&sen.name).unwrap(), sen.voltage, sen.current, sen.power);
-    }
-}
-
-fn prime_connection(port: &mut dyn SerialPort) {
-    println!("Stopping previously started continuous TX");
-    let config = ContTxStruct {
-        enable: pmd::CONFIG_FALSE,
-        timestamp_bytes: pmd::CONFIG_TIMESTAMP_NONE,
-        adc_channels: pmd::CONFIG_MASK_NONE,
-    };
-
-    let config: [u8; size_of::<ContTxStruct>()] = unsafe { transmute(config) };
-
-    match port.write(&config) {
-        Ok(_) => println!("Configured device"),
-        Err(e) => panic!("Error while writing to device: {}", e),
-    }
-
-    port.flush().unwrap();
-
-    thread::sleep(Duration::from_millis(500));
-
-    let mut scratch: Vec<u8> = Vec::new();
-    let l = port.read_to_end(&mut scratch);
-
-    println!("Remaining data: {}", std::str::from_utf8(&scratch).unwrap());
-}
-
-fn read_calibration(port: &mut dyn SerialPort, calibration: &mut [i8; pmd::PMD_ADC_CH_NUM]) {
-    /* Firmware check */
-    match port.write(&[pmd::CMD_READ_ID]) {
-        Ok(_) => println!("Successfully wrote to device"),
-        Err(e) => panic!("Error while writing to device: {}", e),
-    }
-
-    port.flush().unwrap();
-
-    let mut id_buffer = [0u8; size_of::<DeviceIdStruct>()];
-
-    match port.read_exact(&mut id_buffer) {
-        Ok(_) => println!("Received {} bytes", size_of::<DeviceIdStruct>()),
-        Err(e) => panic!("Error while reading from device: {}", e),
-    }
-
-    let device_id: DeviceIdStruct = unsafe {
-        transmute(id_buffer)
-    };
-
-    println!("Running firmware version {}", device_id.firmware);
-
-    /* Get calibration data */
-    match port.write(&[pmd::CMD_READ_CONFIG]) {
-        Ok(_) => println!("Reading config data"),
-        Err(e) => panic!("Error while writing to device: {}", e),
-    }
-
-    port.flush().unwrap();
-
-    if device_id.firmware < 6 {
-        let mut config_buffer = [0u8; size_of::<ConfigStruct>()];
-        match port.read_exact(&mut config_buffer) {
-            Ok(_) => println!("Received config data"),
-            Err(e) => panic!("Error while reading from device: {}", e),
-        }
-        let config: ConfigStruct = unsafe {
-            transmute(config_buffer)
-        };
-        for i in 0..pmd::PMD_ADC_CH_NUM {
-            calibration[i] = config.adc_offset[i];
-        }
-    }
-    else {
-        let mut config_buffer = [0u8; size_of::<ConfigStructV5>()];
-        match port.read_exact(&mut config_buffer) {
-            Ok(_) => println!("Received config data"),
-            Err(e) => panic!("Error while reading from device: {}", e),
-        }
-        let config: ConfigStructV5 = unsafe {
-            transmute(config_buffer)
-        };
-        for i in 0..pmd::PMD_ADC_CH_NUM {
-            calibration[i] = config.adc_offset[i];
-            // TODO what about gain offset?
-        }
-    }
-
-    println!("Calibration data: {:?}", calibration);
+    
 }
--- src/pmd.rs
+++ src/pmd.rs
@@ -1,8 +1,21 @@
+use serialport::SerialPort;
+use std::intrinsics::transmute;
+use std::io::{Read, Write};
+use std::time::Duration;
+use std::{io, thread};
+//use log::{debug};
+
+use crate::pmd;
+
+pub type SensorArray = [u16; PMD_SENSOR_CH_NUM];
+pub type AdcArray = [u16; PMD_ADC_CH_NUM];
+
 pub const CMD_WELCOME_MESSAGE: u8 = 0x00;
 pub const CMD_READ_ID: u8 = 0x01;
 pub const CMD_READ_SENSORS: u8 = 0x02;
-pub const CMD_READ_VALUES: u8 = 0x03;
+pub const CMD_READ_SENSOR_VALUES: u8 = 0x03;
 pub const CMD_READ_CONFIG: u8 = 0x04;
+pub const CMD_WRITE_CONFIG: u8 = 0x05;
 pub const CMD_READ_ADC_BUFFER: u8 = 0x06;
 pub const CMD_WRITE_CONTINUOUS_TX: u8 = 0x07;
 pub const CMD_WRITE_CONFIG_UART: u8 = 0x08;
@@ -10,12 +23,13 @@ pub const CMD_RESET_DEVICE: u8 = 0xF0;
 pub const CMD_ENTER_BOOTLOADER: u8 = 0xF1;
 pub const CMD_NOP: u8 = 0xFF;
 
-pub const PMD_RESPONSE: &[u8; 17] = b"ElmorLabs PMD-USB";
-pub const PMD_SENSOR_NUM: usize = 4;
-pub const PMD_SENSOR_NAME_LEN: usize = 6;
-pub const PMD_SENSOR_BYTE_NUM: usize = 16;
+pub const PMD_WELCOME_RESPONSE: &[u8; 17] = b"ElmorLabs PMD-USB";
 pub const PMD_ADC_CH_NUM: usize = 8;
-pub const PMD_ADC_BYTE_NUM: usize = 16;
+pub const PMD_ADC_BYTE_NUM: usize = size_of::<u16>() * PMD_ADC_CH_NUM;
+pub const PMD_SENSOR_NUM: usize = 4;
+pub const PMD_SENSOR_CH_NUM: usize = 2 * PMD_SENSOR_NUM;
+pub const PMD_SENSOR_NAME_LEN: usize = 6;
+pub const PMD_SENSOR_BYTE_NUM: usize = PMD_ADC_BYTE_NUM;
 
 pub const CONFIG_ENABLE: u8 = 0x01;
 pub const CONFIG_TRUE: u8 = 0x01;
@@ -94,4 +108,177 @@ pub struct UartConfigStruct {
     pub parity: u32,
     pub data_width: u32,
     pub stop_bits: u32,
+}
+
+fn send_command(port: &mut dyn SerialPort, command: u8) {
+    match port.write(&[command]) {
+        Ok(_) => println!("Sending command {:#X} to device", command), // TODO debug!
+        Err(e) => panic!("Error while writing to device: {}", e),
+    }
+
+    port.flush().unwrap();
+}
+
+fn read_response(port: &mut dyn SerialPort, bytes: usize) -> Vec<u8> {
+    let mut buffer = [0u8; bytes]; // TODO this
+    match port.read_exact(&mut buffer) {
+        Ok(_) => {
+            println!("Received response from device");
+            return buffer.to_vec();
+        }
+        Err(e) => panic!("Error while reading from device: {}", e),
+    }
+}
+
+pub fn welcome(port: &mut dyn SerialPort) {
+    send_command(port, CMD_WELCOME_MESSAGE);
+
+    let buffer = read_response(port, PMD_WELCOME_RESPONSE.len());
+
+    assert_eq!(buffer.as_slice(), PMD_WELCOME_RESPONSE);
+    
+    println!("> {}", std::str::from_utf8(&buffer).unwrap()); // TODO debug!
+}
+
+pub fn read_id(port: &mut dyn SerialPort) -> DeviceIdStruct {
+    send_command(port, CMD_READ_ID);
+
+    let mut device_id_buffer = [0u8; size_of::<DeviceIdStruct>()];
+
+    match port.read_exact(&mut device_id_buffer) {
+        Ok(_) => println!("Received {} bytes", size_of::<DeviceIdStruct>()), // TODO debug!
+        Err(e) => panic!("Error while reading from device: {}", e),
+    }
+
+    let device_id: DeviceIdStruct = unsafe { transmute(device_id_buffer) };
+
+    println!("> Running firmware version {}", device_id.firmware); // TODO debug!
+
+    return device_id;
+}
+
+pub fn read_sensors(port: &mut dyn SerialPort) {
+    send_command(port, CMD_READ_SENSORS);
+
+    let mut sensor_buffer = [0u8; size_of::<SensorStruct>()];
+
+    match port.read_exact(&mut sensor_buffer) {
+        Ok(_) => println!("Received {} bytes of data", size_of::<SensorStruct>()), // TODO debug!
+        Err(e) => panic!("Error while reading from device: {}", e),
+    }
+
+    let sensors: SensorStruct = unsafe { transmute(sensor_buffer) };
+
+    for sen in sensors.sensor {
+        println!(
+            "> Sensor {}: Voltage={}, Current={}, Power={}",
+            std::str::from_utf8(&sen.name).unwrap().trim(),
+            sen.voltage,
+            sen.current,
+            sen.power
+        ); // TODO debug!
+    }
+}
+
+pub fn read_sensor_values(port: &mut dyn SerialPort) -> SensorArray {
+    send_command(port, CMD_READ_SENSOR_VALUES);
+
+    let mut sensor_values = [0u8; PMD_SENSOR_BYTE_NUM];
+
+    match port.read_exact(&mut sensor_values) {
+        Ok(_) => println!("Received {} bytes of sensor data", PMD_SENSOR_BYTE_NUM), // TODO debug!
+        Err(e) => panic!("Error while reading from device: {}", e),
+    }
+
+    let sensor_values: [u16; PMD_SENSOR_CH_NUM] = unsafe { transmute(sensor_values) };
+
+    /* PCIE1 voltage (1e-2 V), PCIE1 current (1e-1 A), PCIE2 ... */
+    return sensor_values;
+}
+
+pub fn read_adc_buffer(port: &mut dyn SerialPort) -> AdcArray {
+    send_command(port, CMD_READ_ADC_BUFFER);
+
+    let mut adc_buffer = [0u8; PMD_ADC_BYTE_NUM];
+
+    match port.read_exact(&mut adc_buffer) {
+        Ok(_) => println!("Received {} bytes of sensor data", PMD_ADC_BYTE_NUM), // TODO debug!
+        Err(e) => panic!("Error while reading from device: {}", e),
+    }
+
+    let adc_buffer: [u16; PMD_ADC_CH_NUM] = unsafe { transmute(adc_buffer) };
+
+    return adc_buffer;
+}
+
+pub fn write_config_cont_tx(port: &mut dyn SerialPort, config: &ContTxStruct) {
+    send_command(port, CMD_WRITE_CONTINUOUS_TX);
+    
+    match port.write(&[config.enable, config.timestamp_bytes, config.adc_channels]) {
+        Ok(_) => println!("Configuring device"), // TODO debug!
+        Err(e) => panic!("Error while writing to device: {}", e),
+    }
+
+    port.flush().unwrap();
+
+    println!("Waiting for device to process configuration"); // TODO debug!
+    thread::sleep(Duration::from_secs(1));
+
+    let mut scratch = [0u8; PMD_WELCOME_RESPONSE.len()];
+    match port.read_exact(&mut scratch) {
+        Ok(_) => println!("Device is ready"), // TODO debug!
+        Err(e) => panic!("Error while reading from device: {}", e),
+    }
+
+    println!("> {}", std::str::from_utf8(&scratch).unwrap()); // TODO debug!
+}
+
+pub fn prime_connection(port: &mut dyn SerialPort) {
+    println!("Stopping continuous TX"); // TODO debug!
+    let config = ContTxStruct {
+        enable: CONFIG_FALSE,
+        timestamp_bytes: CONFIG_TIMESTAMP_NONE,
+        adc_channels: CONFIG_MASK_NONE,
+    };
+    write_config_cont_tx(&mut *port, &config);
+}
+
+// TODO read_config(); tricky because of different config versions
+
+// TODO write_config()
+
+pub fn read_calibration(port: &mut dyn SerialPort, calibration: &mut [[i8; 2]; PMD_ADC_CH_NUM]) {
+    let device_id: DeviceIdStruct = read_id(&mut *port);
+
+    match port.write(&[CMD_READ_CONFIG]) {
+        Ok(_) => println!("Reading device config"), // TODO debug!
+        Err(e) => panic!("Error while writing to device: {}", e),
+    }
+
+    port.flush().unwrap();
+
+    if device_id.firmware < 6 {
+        let mut config = [0u8; size_of::<ConfigStruct>()];
+        match port.read_exact(&mut config) {
+            Ok(_) => println!("Received config data"), // TODO debug!
+            Err(e) => panic!("Error while reading from device: {}", e),
+        }
+        let config: ConfigStruct = unsafe { transmute(config) };
+        for i in 0..PMD_ADC_CH_NUM {
+            calibration[i][0] = config.adc_offset[i];
+        }
+    } else {
+        let mut config = [0u8; size_of::<ConfigStructV5>()];
+        match port.read_exact(&mut config) {
+            Ok(_) => println!("Received config data V5"), // TODO debug!
+            Err(e) => panic!("Error while reading from device: {}", e),
+        }
+        let config: ConfigStructV5 = unsafe { transmute(config) };
+        for i in 0..PMD_ADC_CH_NUM {
+            calibration[i][0] = config.adc_offset[i];
+            calibration[i][1] = config.adc_gain_offset[i];
+        }
+    }
+
+    println!("Calibration data: {:?}", calibration); // TODO debug!
 }
